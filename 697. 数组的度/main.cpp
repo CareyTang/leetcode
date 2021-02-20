e4040aec9ab583723492662d075fa69d615dd330
jiawei tang <1026514507@qq.com>
2021-02-20 11:31:24 +0800

    优化算法

--- 697. 数组的度/main.cpp
+++ 697. 数组的度/main.cpp
@@ -20,22 +20,18 @@ class Solution {
 public:
     int findShortestSubArray(vector<int>& nums) {
         map<int,vector<int>> counts{};
-        int ret = nums.size(),max{};
+        int ret = nums.size(),degree{};
         for(int i = 0; i < nums.size();++i){
             if(counts.find(nums[i])==counts.end())counts[nums[i]] = {1,i,i};
             else{
                 counts[nums[i]][0]++;
                 counts[nums[i]][2] = i;
             }
+            degree = std::max(degree,counts[nums[i]][0]);
         }
-        for(const auto& item:counts){
-            if(item.second[0]==max){
-                max = item.second[0];
-                ret = std::min(item.second[2] - item.second[1]+1,ret);
-            }else if(item.second[0]>max){
-                ret = nums.size();
-                max = item.second[0];
-                ret = std::min(item.second[2] - item.second[1]+1,ret);
+        for(const auto& item:counts) {
+            if (item.second[0] == degree) {
+                ret = std::min(item.second[2] - item.second[1] + 1, ret);
             }
         }
         return ret;
